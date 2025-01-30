925e3fc054ed86b9d9d5ecc4b884bc118f74ea06
Zayyan Mustafa <mustafazayyan13@gmail.com>
2025-01-31 03:18:31 +0500

    Just Update UI

--- src/components/userlist.jsx
+++ src/components/userlist.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 export function UserList({ users, selectedUser, onSelectUser }) {
   return (
-    <div className="w-[250px] fix bg-gray-100 p-4 h-auto">
+    <div className="w-[280px] fix bg-gray-100 p-4 h-auto">
       <h2 className="font-bold text-lg">Users</h2>
       <ul className="overflow-y-auto h-auto">
         {users.map((user) => (
           <li
             key={user.id}
-            className={`p-2 cursor-pointer ${selectedUser?.id === user.id ? 'bg-gray-300' : ''}`}
+            className={`p-2 rounded-lg cursor-pointer ${selectedUser?.id === user.id ? 'bg-yellow-300' : ''}`}
             onClick={() => onSelectUser(user)}
           >
-            <img src={user.avatar} alt={user.name} className="inline-block w-8 h-8 rounded-full mr-2" />
+            <img src={user.avatar} alt={user.name} className=" text-white-800 inline-block w-8 h-8 rounded-full mr-2" />
             {user.name}
           </li>
         ))}
